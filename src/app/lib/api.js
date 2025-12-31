3b1e2530b6e1bf4efc73c4903e7d6a56dcb1943a
Kannan <174104295+Vishali28082004@users.noreply.github.com>
2025-12-31 16:50:21 +0530

    env chnages

--- src/app/lib/api.js
+++ src/app/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000'
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://paste-bin-lite-backend-7t5x.vercel.app/'
 
 const apiClient = axios.create({
   baseURL: API_URL,
